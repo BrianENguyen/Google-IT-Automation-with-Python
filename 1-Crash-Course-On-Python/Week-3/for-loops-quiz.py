2d0dce3502cf6bd80d5cf5f747032e1dc7038186
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-22 12:03:19 -0700

    feat: for loops #3

--- 1-Crash-Course-On-Python/Week-3/for-loops-quiz.py
+++ 1-Crash-Course-On-Python/Week-3/for-loops-quiz.py
@@ -14,3 +14,7 @@ def factorial(n):
 
 for n in range(0,9):
     print(n, factorial(n))
+
+# 3. Write a script that prints the first 10 cube numbers (x**3), starting with x=1 and ending with x=10.
+for x in range(1,11):
+  print(x**3)
