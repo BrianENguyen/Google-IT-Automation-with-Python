e52d240487c5de5f0863e32e639fc812d1fd1955
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-22 12:14:37 -0700

    feat: for loops refactor

--- 1-Crash-Course-On-Python/Week-3/for-loops-quiz.py
+++ 1-Crash-Course-On-Python/Week-3/for-loops-quiz.py
@@ -12,7 +12,7 @@ def factorial(n):
         result = result * x
     return result
 
-for n in range(0,9):
+for n in range(0,10):
     print(n, factorial(n))
 
 # 3. Write a script that prints the first 10 cube numbers (x**3), starting with x=1 and ending with x=10.
@@ -21,8 +21,28 @@ for x in range(1,11):
 
 '''
 4. 
+
 Write a script that prints the multiples of 7 between 0 and 100. Print one multiple per line 
 and avoid printing any numbers that aren't multiples of 7. Remember that 0 is also a multiple of 7
 '''
-for i in range(0, 101):
+for i in range(0, 100):
     print(i * 7)
+
+'''
+5. 
+
+The retry function tries to execute an operation that might fail, it retries 
+the operation for a number of attempts. Currently the code will keep executing the 
+function even if it succeeds. Fill in the blank so the code stops trying 
+after the operation succeeded.
+'''
+def retry(operation, attempts):
+  for n in range(attempts):
+    if operation():
+      print("Attempt " + str(n) + " succeeded")
+      break 
+    else:
+      print("Attempt " + str(n) + " failed")
+
+# retry(create_user, 3)
+# retry(stop_service, 5)
