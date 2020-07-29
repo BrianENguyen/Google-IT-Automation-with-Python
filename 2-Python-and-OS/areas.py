48ea8558edb262f3221d697a6e80f6396f506341
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-07-29 11:52:35 -0700

    feat: area of donuts

--- 2-Python-and-OS/areas.py
+++ 2-Python-and-OS/areas.py
@@ -8,3 +8,6 @@ def rectangle(base, height):
 
 def circle(radius):
     return math.pi * (radius ** 2)
+
+def donut(outside_radius, inside_radius):
+    return circle(outside_radius) - circle(inside_radius)
