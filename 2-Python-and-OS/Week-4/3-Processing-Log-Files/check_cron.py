36d303f0e130b68f0ed8dc1f7c0150fb44343317
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-08-01 13:34:39 -0700

    OS week 4: "sense of data"

--- 2-Python-and-OS/Week-4/3-Processing-Log-Files/check_cron.py
+++ 2-Python-and-OS/Week-4/3-Processing-Log-Files/check_cron.py
@@ -1,6 +1,6 @@
 import sys
 import re
-
+usernames = {}
 logfile = sys.argv[1]
 with open(logfile) as file:
     for line in file:
@@ -9,4 +9,8 @@ with open(logfile) as file:
             continue
         pattern = r'USER \((\w+)\)$'
         result = re.search(pattern, line)
-        print(result[1])
+        if result is None:
+            continue
+        name = result[1]
+        usernames[name] = usernames.get(name, 0) + 1
+print(usernames)
