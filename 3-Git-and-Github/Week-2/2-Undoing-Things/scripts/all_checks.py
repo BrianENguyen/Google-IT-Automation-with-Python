0b1127233ccd6388c67545466f39c03d3f94e53f
Brian Nguyen <brian.edison.nguyen@gmail.com>
2020-08-14 12:23:06 -0700

    test file for undoing

---
+++ 3-Git-and-Github/Week-2/2-Undoing-Things/scripts/all_checks.py
@@ -0,0 +1,13 @@
+import os, sys
+
+def check_reboot():
+    '''Returns true if the computer has a pending reboot'''
+    return os.path.exists('/run/reboot-required')
+
+def main():
+    if check_reboot():
+        print('Pending reboot')
+        sys.exit(1)
+    print('Everything ok')
+    sys.exit(0)
+main()
